0bebddb80b2bfffb579ae1b8f3c8c0a03bb2a3b1
RodrigoRamos <rodrigoramosnajera@gmail.com>
2016-05-18 19:05:21 -0600

    Ctlrs.

---
+++ app/controllers/Boletos.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.With;
+
+/**
+ * Controlador para los boletos
+ * 
+ * @author Rodrigo Ramos Najera
+ */
+@With(Secure.class)
+public class Boletos extends CRUD{
+    
+}
--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -1,9 +1,5 @@
 package controllers;
 
-import models.Usuario;
-import org.hibernate.annotations.Check;
-import play.mvc.Before;
-import play.mvc.Router;
 import play.mvc.With;
 
 /**
